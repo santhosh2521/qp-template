bfd7aeb6265cc6c7d5dab06724eba96b3edb7f20
santhosh2521 <santhosh600sk@gmail.com>
2024-03-11 14:01:39 +0530

    final update had some problem in signup css

--- frontend/src/select-template.js
+++ frontend/src/select-template.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 // import './home.css';
-=======
+
 import './Popup.css';
 import './home.css';
 
